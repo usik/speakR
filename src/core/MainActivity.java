0d6d6f4186f5a88830b6e09af5a49f7cb53ba4fe
usik <usik.o2@gmail.com>
2015-02-20 16:55:33 -0500

    Added packages and blank files
    Initializing folder trees

---
+++ src/core/MainActivity.java
@@ -0,0 +1,10 @@
+package core;
+//This is the main activity
+/*
+ * TODO
+ * 1. Create layouts of screens
+ * 2. Code!
+ */
+public class MainActivity {
+
+}
---
+++ src/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package login;
+// This is a log in activity.
+/*
+ * TODO
+ * 1. Create log in layout
+ * 2. Code log in activity
+ * 3. Create sign in layout
+ * 4. Code sign in activity
+ * 5. Create log out layout - is it needed?
+ * 6. Link to database
+ * 
+ */
+public class LoginActivity {
+
+}
